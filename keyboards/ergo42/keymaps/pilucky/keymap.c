b213331db52bc341ae0d84303900c972b206b643
pilucky <pilucky1192@gmail.com>
2019-08-13 16:31:48 +0900

    -

--- keyboards/ergo42/keymaps/pilucky/keymap.c
+++ keyboards/ergo42/keymaps/pilucky/keymap.c
@@ -25,10 +25,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `------------------------------------------------'   `------------------------------------------------'
    */
   [BASE] = LAYOUT( \
-    KC_TAB,          KC_Q,    KC_W,    KC_E,    KC_R,            KC_T,            KC_GRV,             KC_RBRC,          KC_Y,            KC_U,            KC_I,    KC_O,    KC_P,     KC_BSPC, \
+    LT(SYMB, KC_TAB),          KC_Q,    KC_W,    KC_E,    KC_R,            KC_T,            KC_GRV,             KC_RBRC,          KC_Y,            KC_U,            KC_I,    KC_O,    KC_P,     KC_BSPC, \
     KC_LCTL,         KC_A,    KC_S,    KC_D,    KC_F,            KC_G,            KC_MINS,            KC_LBRC,          KC_H,            KC_J,            KC_K,    KC_L,    KC_SCLN,  KC_ENT, \
     KC_LSFT,         KC_Z,    KC_X,    KC_C,    KC_V,            KC_B,            KC_EQL,             KC_BSLS,          KC_N,            KC_M,            KC_COMM, KC_DOT,  KC_SLSH,  KC_QUOT, \
-    LT(SYMB, KC_0),  KC_LALT, S(KC_2), S(KC_1), KC_LGUI,         LT(META,KC_SPC), LT(META,KC_ESC),    LT(SYMB,KC_SPC),  LT(META,KC_SPC), KC_RGUI,         KC_LEFT, KC_DOWN, KC_UP,    KC_RGHT \
+    LT(SYMB, KC_0),  KC_LALT, S(KC_2), LT(SYMB, S(KC_1)), KC_LGUI,         LT(META,KC_SPC), LT(META,KC_ESC),    LT(SYMB,KC_SPC),  LT(META,KC_SPC), KC_RGUI,         KC_LEFT, KC_DOWN, KC_UP,    KC_RGHT \
   ),
 
   /* META
